7f8c2eebae545470f771c9c14ebc9e7e94c0140b
dfinke <finked@hotmail.com>
2017-04-04 20:54:24 -0400

    Renamed

--- New-BlogPost.ps1
+++ New-BlogPost.ps1
@@ -1,48 +1,44 @@
-﻿function New-BlogPost {
-    param(
-        [parameter(Mandatory)]
-        $name,
-        $title,
-        $description,
-        [ValidateSet('Post','Draft')]
-        $PostType="Draft"
-    )
+param(
+    [parameter(Mandatory)]
+    $title,
+    $tag,
+    [ValidateSet('draft', 'post')]
+    $postType = "draft"
+)
 
-    $basePath = "C:\Users\Douglas\Documents\GitHub\dfinke.github.io\"
-    $postPath = "$($basePath)\_drafts"
-    if($PostType -eq "post") {
-        $postPath = "$($basePath)\_posts"
-    }
+$tags = @('PowerShell')
+if ($tag) {
+    $tags += $tag
+    $tags = $tags -join ','
+}
 
-    $date = Get-Date
-    
-    $fileName = "{0}-{1}.md" -f $date.ToString("yyyy-MM-dd"),$name
-    $fullName =  "$($postPath)\$($fileName)"
-    
-    $postDate = $date.ToString("yyyy-MM-dd HH:mm:ss")
-
-    if(!$title) {
-        $title=$name
-    }
-
-    if(!$description) {
-        $description=$name
-    }
-    
-    $init = @"
+$content = @"
 ---
 layout: post
-title:  "$title"
-date:   $postDate
+title:  "$($title)"
+date:   $((get-date).tostring("u") -replace 'z$','')
 comments: true
-description: $description
+description: "$($title)"
 categories: 
     - PowerShell
 tags: 
-    - PowerShell
-# thumbnail
+    - $($tags)
 ---
 "@
 
-    $init  | Set-Content $fullName -Encoding Ascii
+$fileName = ("{0}-{1}.md" -f (Get-Date).ToString("yyyy-MM-dd"), ($title -replace ",| ", "-")) -replace "--", "-"
+
+$basepath = "C:/Users/Douglas/Documents/GitHub/dfinke.github.io" 
+
+switch ($postType) {
+    "post" {
+        $targetPath = "_posts"
+    }
+    "draft" {
+        $targetPath = "_drafts"
+    }
 }
+    
+$fileName = "$($basepath)/$($targetPath)/$($fileName)"
+    
+$content | Set-Content -Encoding Ascii $fileName
